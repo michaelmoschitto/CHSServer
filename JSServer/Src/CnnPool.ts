068e05ae1716c885526ff594975acbf9a685cda1
Michael Moschitto <michaelmoschitto@gmail.com>
2021-02-15 21:42:19 -0800

    "Say hello to my little friend! -Tony Montana CnnPool converted to ts and all tests pass"

--- JSServer/Src/CnnPool.ts
+++ JSServer/Src/CnnPool.ts
@@ -1,6 +1,8 @@
-'use strict'
+import { queryCallback, PoolConnection, MysqlError, createPool, Pool } from 'mysql';
+import {default as PoolCFG} from './connection.json'
+import { Router } from 'express';
+import {Request, Response, NextFunction} from 'express-serve-static-core';
 
-var mysql = require('mysql');
 
 // Constructor for DB connection pool
 // Note: In some mysql server instances, standard password access may
@@ -8,52 +10,71 @@ var mysql = require('mysql');
 // in question via this MySQL command, replacing the <> with appropriate
 // values.
 // ALTER USER '<user>'@'localhost' IDENTIFIED WITH mysql_native_password BY '<YourPassword>'; 
-var CnnPool = function() {
-   var poolCfg = require('./connection.json');
+// var CnnPool = function() {
+//    var poolCfg = require('./connection.json');
 
-   poolCfg.connectionLimit = CnnPool.PoolSize;
-   this.pool = mysql.createPool(poolCfg);
+//    poolCfg.connectionLimit = CnnPool.PoolSize;
+//    this.pool = mysql.createPool(poolCfg);
 
-   console.log(poolCfg);
+//    console.log(poolCfg);
+// };
+interface poolConfig{
+   connectionLimit?: number;
+   host?: string;
+   user?: string;
+   password?: string;
 };
 
-// NOTE: Do *not* change this pool size.  It is required to be 1 in order
-// to demonstrate you are properly freeing connections!
-CnnPool.PoolSize = 1;
+export let router = Router({ caseSensitive: true });
 
-// The one (and probably only) CnnPool object needed for the app
-CnnPool.singleton = new CnnPool();
+export class CnnPool{
+   
+   // NOTE: Do *not* change this pool size.  It is required to be 1 in order
+   // to demonstrate you are properly freeing connections!
+   static PoolSize = 1;
 
-// Conventional getConnection, drawing from the pool
-CnnPool.prototype.getConnection = function(cb) {
-   this.pool.getConnection(cb);
-};
+   poolCfg: poolConfig = PoolCFG; 
+   pool: Pool;
 
-// Router function for use in auto-creating CnnPool for a request
-CnnPool.router = function(req, res, next) {
-   console.log("Getting connection");
-   CnnPool.singleton.getConnection(function(err, cnn) {
-      if (err)
+   constructor(){
+      this.poolCfg.connectionLimit = CnnPool.PoolSize;    
+      this.pool = createPool(this.poolCfg);
+      console.log(this.poolCfg);  
+   }
+   
+   // The one (and probably only) CnnPool object needed for the app
+   static singleton = new CnnPool();
+   
+   
+   // Conventional getConnection, drawing from the pool
+   getConnection = (cb: queryCallback) => {
+      this.pool.getConnection(cb);
+   };
+   
+   // Router function for use in auto-creating CnnPool for a request
+   static router = (req: Request, res: Response, next: NextFunction) => {
+      console.log("Getting connection");
+      CnnPool.singleton.getConnection(function(err, cnn) {
+         if (err)
          res.status(500).json('Failed to get connection ' + err);
-      else {
-         console.log("Connection acquired");
-         cnn.chkQry = function(qry, prms, cb) {
-            // Run real qry, checking for error
-            // console.log(res);
-
-            // res is the reponse from the DATABASE!!!!!!! NOT THE HTTP REPONSE
-            this.query(qry, prms, function(err, dbRes, fields) {
-               if (err){
-                  res.status(500).json('Failed query ' + qry);
-                  console.log(err);
-               }
-               cb(err, dbRes, fields);
-            });
-         };
-         req.cnn = cnn;
-         next();
-      }
-   });
-};
+         else {
+            console.log("Connection acquired");
+            cnn.chkQry = function(qry: string, prms: string[], cb: queryCallback) {
+               // Run real qry, checking for error
+               
+               this.query(qry, prms, function(err: any, dbRes: string, fields: any) {
+                  if (err){
+                     res.status(500).json('Failed query ' + qry);
+                     console.log(err);
+                  }
+                  cb(err, dbRes, fields);
+               });
+            };
+            req.cnn = cnn;
+            next();
+         }
+      });
+   };
+} //class closing brace
 
 module.exports = CnnPool;
--- JSServer/built/CnnPool copy.js
+++ JSServer/built/CnnPool copy.js
@@ -1,34 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CnnPool = exports.router = void 0;
-const mysql_1 = require("mysql");
-const connection_json_1 = __importDefault(require("./connection.json"));
-const express_1 = require("express");
-;
-exports.router = express_1.Router({ caseSensitive: true });
-class CnnPool {
-    constructor() {
-        this.poolCfg = connection_json_1.default;
-        // Conventional getConnection, drawing from the pool
-        this.getConnection = (cb) => {
-            this.pool.getConnection(cb);
-        };
-        this.poolCfg.connectionLimit = CnnPool.PoolSize;
-        this.pool = mysql_1.createPool(this.poolCfg);
-        console.log(this.poolCfg);
-    }
-} //class closing brace
-exports.CnnPool = CnnPool;
+// Constructor for DB connection pool
+// Note: In some mysql server instances, standard password access may
+// not be the default authentication.  In this case, adjust the user
+// in question via this MySQL command, replacing the <> with appropriate
+// values.
+// ALTER USER '<user>'@'localhost' IDENTIFIED WITH mysql_native_password BY '<YourPassword>'; 
+var CnnPool = function () {
+    var poolCfg = require('./connection.json');
+    poolCfg.connectionLimit = CnnPool.PoolSize;
+    this.pool = mysql.createPool(poolCfg);
+    console.log(poolCfg);
+};
 // NOTE: Do *not* change this pool size.  It is required to be 1 in order
 // to demonstrate you are properly freeing connections!
 CnnPool.PoolSize = 1;
 // The one (and probably only) CnnPool object needed for the app
 CnnPool.singleton = new CnnPool();
+// Conventional getConnection, drawing from the pool
+CnnPool.prototype.getConnection = function (cb) {
+    this.pool.getConnection(cb);
+};
 // Router function for use in auto-creating CnnPool for a request
-CnnPool.router = (req, res, next) => {
+CnnPool.router = function (req, res, next) {
     console.log("Getting connection");
     CnnPool.singleton.getConnection(function (err, cnn) {
         if (err)
@@ -37,6 +31,8 @@ CnnPool.router = (req, res, next) => {
             console.log("Connection acquired");
             cnn.chkQry = function (qry, prms, cb) {
                 // Run real qry, checking for error
+                // console.log(res);
+                // res is the reponse from the DATABASE!!!!!!! NOT THE HTTP REPONSE
                 this.query(qry, prms, function (err, dbRes, fields) {
                     if (err) {
                         res.status(500).json('Failed query ' + qry);
