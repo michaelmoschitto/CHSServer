c07650c3928bb809ec3b7936610afa32f08e15bc
Michael Moschitto <michaelmoschitto@gmail.com>
2021-03-15 13:40:39 -0700

    initial commit before starting signIn conversion to functional component

--- reactclient/src/components/Likes/Likes.jsx
+++ reactclient/src/components/Likes/Likes.jsx
@@ -2,28 +2,32 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import {useSelector} from 'react-redux';
 
+const LikedBy = props => {
+   // const msgId = props.msgId
+   const likes = useSelector(store => store.Likes);
 
-const LikedBy = (props) => {
-    // const msgId = props.msgId
-    const likes = useSelector(store => store.Likes);
-    
-    let divs = [];
-    (function(){
-    
-        likes && likes[props.msgId] && likes[props.msgId].forEach((l) => 
-         divs.push(<div>{`${l.firstName} ${l.lastName}`}</div>))
-    })();
+   let divs = [];
+   (function () {
+      likes && likes[props.msgId] && 
+       likes[props.msgId].forEach(l =>
+       divs.push(<div>{`${l.firstName} ${l.lastName}`}</div>));
+   })();
 
-    return (
-        <Popup trigger={<div onClick={() => props.likeMsg()}
-         >{divs.length + ' Likes'}</div>} 
-          on="hover" position="right" arrow={true} 
-         onOpen={()=>{props.getLikes()}}
-         >
-    
-            {divs.length ? divs : ''}
-        </Popup>
-    );
+   return (
+      <Popup
+         trigger={
+            <div onClick={() => props.likeMsg()}>{divs.length + ' Likes'}</div>
+         }
+         on="hover"
+         position="right"
+         arrow={true}
+         onOpen={() => {
+            props.getLikes();
+         }}
+      >
+         {divs.length ? divs.slice(0, 5) : ''}
+      </Popup>
+   );
 };
 
 export default LikedBy;
